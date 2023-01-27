ee5ba63f15dc66cba4d70067a4e335042a4e6577
aleksem07 <aleksem07@gmail.com>
2023-01-27 09:13:46 +0300

    fix: add preview small

--- js/upload-preview.js
+++ js/upload-preview.js
@@ -1,7 +1,13 @@
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 
+// const PictureSize = {
+//   WIDTH: 600,
+//   HEIGHT: 600,
+// };
+
 const fileChooser = document.querySelector('#upload-file');
 const preview = document.querySelector('.img-upload__preview img');
+const previewSmall = document.querySelectorAll('.effects__preview');
 
 fileChooser.addEventListener('change', () => {
   const file = fileChooser.files[0];
@@ -15,6 +21,12 @@ fileChooser.addEventListener('change', () => {
 
     reader.addEventListener('load', () => {
       preview.src = reader.result;
+      // preview.width = PictureSize.WIDTH;
+      // preview.height = PictureSize.HEIGHT;
+
+      previewSmall.forEach((filter) => {
+        filter.style.backgroundImage = `url(${reader.result})`;
+      });
     });
     reader.readAsDataURL(file);
   }
